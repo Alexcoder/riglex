4abf78dff308b7a82310224e6be8974b33472d8d
Alexcoder <agboalez@gmail.com>
2023-01-25 07:13:33 +0100

    refactor bump pressure

--- src/components/calcPage/BumpPressure/SingleBumpPressure.js
+++ src/components/calcPage/BumpPressure/SingleBumpPressure.js
@@ -3,11 +3,23 @@ import {TextField, Grid} from '@mui/material';
 
 const SingleBumpPressure =({label, onChange, value, name})=>{
     return(
+        <Grid item xs={12} sm={6} md={3} 
+        sx={{padding:"1rem 0.5rem 1rem 0.5rem",display:"flex", justifyContent:"center", textAlign:"start",alignItems:"center", width:"100%", border:"0.5px solid lightgray"}}
+      >    
+       <div style={{width:"100%", }}>
+        <Typography sx={{width:"100%"}}>{label}</Typography>
+        </div>
+           <TextField  
+            sx={{padding: "0.1rem", width:"50%", height:"3rem"}}
+              variant="outlined"
+              color="primary"
+              name={name}
+              type='number' 
+              value={value} 
+              onChange={onChange} />
+     </Grid>
 
-        <Grid item xs={12} sm={12} md={4}>
-            <TextField variant="filled" type="number" label={label} onChange={onChange} value={value} name={name}
-            sx={{width: "17rem"}}/>
-        </Grid>
+
     )
 }
 
