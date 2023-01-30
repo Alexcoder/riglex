3a643a333f2edb4af5d26ceeda9d820b215ff74d
Alexcoder <agboalez@gmail.com>
2023-01-30 20:43:51 +0100

    refactor additive css

--- src/components/Additive/Additive.js
+++ src/components/Additive/Additive.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import SingleAdditive from './SingleAdditive';
-import { Grid,  } from '@mui/material';
 import './Additive.css';
 
 const Additive = () => {
@@ -39,72 +38,97 @@ const Additive = () => {
 
 
   return (
-  <div style={{width: "100vw"}}>
-     <Grid container justifyContent="center" alignItems="center" textAlign="center"
-     rowSpacing={1} sx={{width: "90%",}}
-      > 
-      <Grid item xs={10} sm={10} md={10}>
-      <h2 style={{color: "blue", textAlign: "center", marginBottom: "0.2rem"}}> Additive Check </h2>
-      </Grid>
+  <main className="additive-container">
+     <div> 
+         <div>
+            <h2 className="additive-check">
+                Additive Check 
+            </h2>
+         </div>
 
-      { sacksOfCement ? null :
-      <>
-      <SingleAdditive value={slurryVolume} label={`Slurry Volume (bbl)`} 
-      onChange={(e)=> setSlurryVolume(e.target.value)}
-      />
-       <SingleAdditive value={slurryYield} label={`Slurry Yield (ft3/sk)`} 
-       onChange={(e)=> {setSlurryYield(e.target.value)}}/>
-      </>
-      }
+         { 
+          sacksOfCement 
+          ? null 
+          :
+         <div> 
+            <SingleAdditive 
+               value={slurryVolume} 
+               label={`Slurry Volume (bbl)`} 
+               onChange={(e)=> setSlurryVolume(e.target.value)}/>
+            <SingleAdditive 
+               value={slurryYield} 
+               label={`Slurry Yield (ft3/sk)`} 
+               onChange={(e)=> {setSlurryYield(e.target.value)}}/>
+        </div>
+        }
 
-      {      //CONDITIONAL RENDERING */}
-       slurryYield || slurryVolume? null :
-       <SingleAdditive value={sacksOfCement} label={`Sacks Of Cement`} 
-       onChange={(e)=> {setSacksOfCement(e.target.value)}}
-       /> 
-       }
+        {      //CONDITIONAL RENDERING */}
+         slurryYield || slurryVolume
+         ? null 
+         :
+          <SingleAdditive 
+               value={sacksOfCement} 
+               label={`Sacks Of Cement`} 
+              onChange={(e)=> {setSacksOfCement(e.target.value)}} /> 
+        }
 
-       <SingleAdditive value={mixFluidConc} label={`Mixfluid Concentration (gal/sk)`} 
-        onChange={(e)=> {setMixFluidConc(e.target.value)}} 
-        />
-       <SingleAdditive value={deadVolume} label={`Tank Dead Volume (bbl)`} 
-       onChange={(e)=> {setDeadVolume(e.target.value)}}
-       />
-       <SingleAdditive value={additiveConcentration} label={`Additive Concentration (gal/sk)`} 
-       onChange={(e)=> {setAdditiveConcentration(e.target.value)}} 
-       />
-
+        <SingleAdditive 
+           value={mixFluidConc} 
+           label={`Mixfluid Concentration (gal/sk)`} 
+           onChange={(e)=> {setMixFluidConc(e.target.value)}}/>
+        <SingleAdditive 
+           value={deadVolume} 
+           label={`Tank Dead Volume (bbl)`} 
+           onChange={(e)=> {setDeadVolume(e.target.value)}}/>
+       <SingleAdditive 
+           value={additiveConcentration} 
+           label={`Additive Concentration (gal/sk)`} 
+           onChange={(e)=> {setAdditiveConcentration(e.target.value)}}/>
        {/* -----------RESULT-------------- */}
 
        {
-        slurryVolume>"0" & slurryYield>"0" & mixFluidConc>="0" & deadVolume>="0" & additiveConcentration>"0" ?
-        <Grid item xs={10} sm={10} md={10}>
-        <h3 style={{textAlign: "center", color: "blue",}}>Additive 
-       <span style={{color: "red"}}> {Additive} </span>  
-        gallon</h3>
-        </Grid>
-        :sacksOfCement>"0" & mixFluidConc>="0" & deadVolume>="0" & additiveConcentration>"0" ?
-        <Grid item xs={10} sm={10} md={10}>
-        <h3 style={{textAlign: "center", color: "blue",}}>Additive 
-        <span style={{color: "red"}}> {Additive} </span>  
-         gallon</h3>
-         </Grid>
+         slurryVolume>"0"
+         & slurryYield>"0" 
+         & mixFluidConc>="0" 
+         & deadVolume>="0" 
+         & additiveConcentration>"0" 
+         ?
+         <div>
+            <h3 style={{textAlign: "center", color: "blue",}}>Additive 
+              <span style={{color: "white"}}> {Additive} </span>  
+              gallon
+            </h3>
+        </div>
+        :
+         sacksOfCement>"0" 
+         & mixFluidConc>="0" 
+         & deadVolume>="0" 
+         & additiveConcentration>"0" 
+         ?
+         <div>
+            <h3 style={{textAlign: "center", color: "blue",}}>Additive 
+              <span style={{color: "red"}}> {Additive} </span>  
+               gallon
+            </h3>
+         </div>
          : null
        }
-       <Grid item xs={10} sm={10} md={10}>
-       <h4 style={{textAlign: "center", color: "red", marginTop: "-0.5rem"}}>{MixFluid_With_DeadVolume_Changer}</h4>
-       </Grid>
+       <div>
+            <h4 style={{textAlign: "center", color: "white"}}>
+               {MixFluid_With_DeadVolume_Changer}
+            </h4>
+       </div>
       
-      <Grid item xs={12} sm={10} md={10}>
-       <button className= "additive_button" onClick={clear}>CLEAR</button>
-       </Grid>
+       <div>
+         <button 
+            className= "additive-clear" 
+            onClick={clear}>
+               CLEAR
+         </button>
+       </div>
 
-      <Grid item xs={10} sm={10} md={10}>
-      <div className="lexwares" >Lexwares @2022</div>
-      </Grid>
-
-    </Grid>
-  </div>
+    </div>
+  </main>
   )
 }
 
--- src/components/Additive/SingleAdditive.js
+++ src/components/Additive/SingleAdditive.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Grid, TextField } from '@mui/material';
+import { Grid, TextField, Typography } from '@mui/material';
 
 const SingleAdditive = ({label,value,onChange}) => {
 
   return (
-    <Grid item xs={12} sm={10} md={12}>
+    <Grid 
+      sx={{
+           display:"flex", 
+           background:"white",
+           justifyContent:"space-between",
+           padding:"0.5rem 2rem",
+           border:"0.1px solid lightgray",
+         }}>
+      <Typography 
+         sx={{
+             marginTop:"1rem", 
+             textAlign:"start"
+             }}>
+              {label}
+      </Typography>
       <TextField
-      sx={{width: "17rem", height: "3.5rem"}}
-      type='number' variant="outlined"
-      value={value} 
-      label={label} 
-      onChange={onChange}
+           sx={{width: "30%",}}
+           type='number' 
+           variant="outlined"
+           value={value} 
+           onChange={onChange}
       />
     </Grid>
   )
